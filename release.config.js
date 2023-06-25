efa821b66f3b8d91e4bc04f3c340e6357a8fa9a5
Idel Gataullin <idel28102001@mail.ru>
2023-06-25 10:33:40 +0300

    fix: d12331

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const config = {
     branches: ['main'],
     plugins: [
-        // '@semantic-release/commit-analyzer',
+        '@semantic-release/commit-analyzer',
         // '@semantic-release/release-notes-generator',
         [
             "@semantic-release/changelog",
