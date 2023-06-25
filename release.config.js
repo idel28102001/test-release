f36ccdef62eda0cd841c1f77c40cde6fd6269dcf
Idel Gataullin <idel28102001@mail.ru>
2023-06-25 09:59:27 +0300

    ci:dasdasdas

--- release.config.js
+++ release.config.js
@@ -10,8 +10,7 @@ const config = {
             }
         ],
         ['@semantic-release/git', {
-            "assets": ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            "assets": ["docs/CHANGELOG.md"],
         }],
         '@semantic-release/github'
     ]
