8955db031d1b0987538ec20b9e72563cf626cdb7
Idel Gataullin <idel28102001@mail.ru>
2023-06-25 09:54:32 +0300

    ci: added changelog

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ const config = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
         ['@semantic-release/git', {
             "assets": ["dist/*.js", "dist/*.js.map"],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
